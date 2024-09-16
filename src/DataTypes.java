d0a8efd392f76ca84eeb302d202a55131b3a93eb
Tanaya <tanaya.datar@mail.utoronto.ca>
2024-09-16 15:52:14 -0400

    correct version; bugs fixed

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,10 +1,10 @@
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+    // to do TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
@@ -12,3 +12,5 @@ public class DataTypes {
         return s;
     }
 }
+
+// check lab 2 other file for og code + explanation on why this code works
